8e68b42b76b6117c53e7723ea9ebae712bab8c8c
esperanzzza <veronikatelychko@yahoo.com>
2016-09-10 00:50:38 +0300

    Исправила найденные ошибки

--- js/showhide-tabs-component.js
+++ js/showhide-tabs-component.js
@@ -14,8 +14,12 @@ ShowHideTabs.prototype._closeElement = function (e) {
     this.elForShow.forEach(function (item, i) {
       if (i !== this.numForHide) {
         this.elForShow[i].hidden = true;
+        this.elForClick[i].style.background = '#32425c';
+        this.elForClick[i].style.color = '#FFFFFF';
+      } else {
+        this.elForClick[i].style.background = '#FFFFFF';
+        this.elForClick[i].style.color = '#32425c';
       }
     }.bind(this))
   }
 };
-
